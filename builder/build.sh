ad7581e2ddb79c89e63203200d2aa0f40e9972d3
Evangelos Foutras <evangelos@foutrelis.com>
2015-12-28 22:49:11 +0200

    Prepare Ruby 2.3.0 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -85,7 +85,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitcmd='svn commit -m "boost 1.60.0 rebuild"'
+	commitcmd='svn commit -m "Ruby 2.3.0 rebuild"'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		# multilib package with i686 variant
