31466760a0b37d1c6a0eec9e95e8ead6a1fcc7b4
Evangelos Foutras <evangelos@foutrelis.com>
2016-12-17 18:00:32 +0200

    Prepare imagemagick 6.9.6.8 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -85,7 +85,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitcmd='svn commit -m "poppler 0.50.0 rebuild"'
+	commitcmd='svn commit -m "imagemagick 6.9.6.8 rebuild"'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		# multilib package with i686 variant
