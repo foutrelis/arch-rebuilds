fbdf6ba3bd4aa145fdfeccbf82b362ad3567bdca
Evangelos Foutras <evangelos@foutrelis.com>
2017-05-31 19:26:29 +0300

    Prepare Perl 5.26 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -85,7 +85,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitcmd='svn commit -m "MariaDB 10.2.6 rebuild"'
+	commitcmd='svn commit -m "Perl 5.26 rebuild"'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		# multilib package with i686 variant
