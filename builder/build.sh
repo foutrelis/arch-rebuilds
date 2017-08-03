51c988c11cffd12d77758350d82b47c6ffa9508d
Evangelos Foutras <evangelos@foutrelis.com>
2017-08-03 19:41:00 +0300

    Prepare GLEW 2.1.0 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -85,7 +85,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitcmd='svn commit -m "poppler 0.57.0 rebuild"'
+	commitcmd='svn commit -m "GLEW 2.1.0 rebuild"'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		# multilib package with i686 variant
