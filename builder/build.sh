15803d1e2bdb9928688b653913d2417c81e5f093
Evangelos Foutras <evangelos@foutrelis.com>
2018-11-17 16:01:53 +0200

    Prepare hunspell 1.7.0 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -81,7 +81,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitcmd='svn commit -m "PIE/BUILDINFO rebuild"'
+	commitcmd='svn commit -m "hunspell 1.7.0 rebuild"'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
