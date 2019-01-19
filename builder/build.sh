11e6aa49c0301444f5dc720d5e6cc72d6fe93fed
Evangelos Foutras <evangelos@foutrelis.com>
2019-01-19 12:49:16 +0200

    Prepare poppler 0.73.0 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -81,7 +81,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitcmd='svn commit -m "MariaDB 10.3 rebuild"'
+	commitcmd='svn commit -m "poppler 0.73.0 rebuild"'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
