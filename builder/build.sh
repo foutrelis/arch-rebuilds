7fff1eac5f6ebd55b567ef53e85412efcd061d96
Evangelos Foutras <evangelos@foutrelis.com>
2019-03-28 17:31:30 +0200

    Prepare icu 64.1 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -81,7 +81,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitcmd='svn commit -m "poppler 0.75.0 rebuild"'
+	commitcmd='svn commit -m "icu 64.1 rebuild"'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
