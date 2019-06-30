a8a445073baf66cf51168300edb56892599a114b
Evangelos Foutras <evangelos@foutrelis.com>
2019-06-30 12:32:43 +0300

    Prepare poppler 0.78 / nettle 3.5 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -81,7 +81,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitcmd='svn commit -m "Perl 5.30 rebuild"'
+	commitcmd='svn commit -m "poppler 0.78 / nettle 3.5 rebuild"'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
