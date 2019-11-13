243eeb7173c1ea5100a3043c4611d56097846fcc
Evangelos Foutras <evangelos@foutrelis.com>
2019-11-13 16:58:16 +0200

    Prepare [core] mass rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -81,7 +81,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitcmd='svn commit -m "icu 65.1 / poppler 0.82.0 rebuild"'
+	commitcmd='svn commit -m "[core] mass rebuild"'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
