82c93f08ba39a7341a943e0ac949b3255783cde5
Evangelos Foutras <evangelos@foutrelis.com>
2019-12-25 23:22:07 +0200

    Prepare Ruby 2.7.0 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -81,7 +81,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitcmd='svn commit -m "poppler 0.83.0 rebuild"'
+	commitcmd='svn commit -m "Ruby 2.7.0 rebuild"'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
