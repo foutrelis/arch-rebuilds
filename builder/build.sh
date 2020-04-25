a91d9d8c47214a07389ec8c48b8c5138eab49a1a
Evangelos Foutras <evangelos@foutrelis.com>
2020-04-25 11:52:47 +0300

    Prepare glew 2.2.0 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -81,7 +81,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitcmd='svn commit -m "libffi 3.3 rebuild"'
+	commitcmd='svn commit -m "glew 2.2.0 rebuild"'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
