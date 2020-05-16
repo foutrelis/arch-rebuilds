99d3cfe4266977f3a3e8a19746646aff390430ce
Evangelos Foutras <evangelos@foutrelis.com>
2020-05-16 10:59:08 +0300

    Prepare [extra] reproducibility rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -81,7 +81,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitmsg='nettle 3.6 rebuild'
+	commitmsg='reproducibility rebuild'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
