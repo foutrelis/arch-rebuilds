d2479dec66f4fcc1056d4cb42c876f8afc5b986e
Evangelos Foutras <evangelos@foutrelis.com>
2020-10-04 23:32:23 +0300

    Prepare poppler 20.10.0 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -81,7 +81,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitmsg='poppler 20.08.0 rebuild'
+	commitmsg='poppler 20.10.0 rebuild'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
