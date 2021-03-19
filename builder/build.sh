da0c9487f9754cc026b3127c0b0d84c7dfc2039e
Evangelos Foutras <evangelos@foutrelis.com>
2021-03-19 04:12:57 +0200

    Prepare Ruby 3.0.0 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -88,7 +88,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitmsg='protobuf 3.15.6 rebuild'
+	commitmsg='Ruby 3.0.0 rebuild'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
