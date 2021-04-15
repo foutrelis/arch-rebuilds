1c509a735088dfe23e4e16371f5a15676887c100
Evangelos Foutras <evangelos@foutrelis.com>
2021-04-15 14:00:02 +0300

    Prepare icu 69.1 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -88,7 +88,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitmsg='Ruby 3.0.0 rebuild'
+	commitmsg='icu 69.1 rebuild'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
