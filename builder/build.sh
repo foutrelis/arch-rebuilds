4418c366d5239f375787386361493db86f44c911
Evangelos Foutras <evangelos@foutrelis.com>
2021-05-08 14:30:43 +0300

    Prepare protobuf 3.16.0 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -88,7 +88,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitmsg='poppler 21.05.0 rebuild'
+	commitmsg='protobuf 3.16.0 rebuild'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
