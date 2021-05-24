29d5a965da58dcff91458387915e2769acd5ad8a
Evangelos Foutras <evangelos@foutrelis.com>
2021-05-24 11:12:49 +0300

    Prepare Perl 5.34 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -88,7 +88,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitmsg='imath/openexr 3.0.2 rebuild'
+	commitmsg='Perl 5.34 rebuild'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
