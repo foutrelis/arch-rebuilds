b4816e377ea35e762826d167f333d1b41db3d32c
Evangelos Foutras <evangelos@foutrelis.com>
2021-08-01 20:37:44 +0300

    Prepare poppler 21.08.0 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -88,7 +88,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitmsg='protobuf 3.17.3 rebuild'
+	commitmsg='poppler 21.08.0 rebuild'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
