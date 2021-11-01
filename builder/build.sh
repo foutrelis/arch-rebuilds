4f7cf90a39ef6ae993c2d8658aff35e8c8fcf828
Evangelos Foutras <evangelos@foutrelis.com>
2021-11-01 19:57:51 +0200

    Prepare poppler 21.11.0 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -88,7 +88,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitmsg='libffi 3.4 rebuild'
+	commitmsg='poppler 21.11.0 rebuild'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
