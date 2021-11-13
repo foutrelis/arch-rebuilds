119bd23cde8bb656a4682557b5d3808124d37209
Evangelos Foutras <evangelos@foutrelis.com>
2021-11-13 10:57:57 +0200

    Prepare openldap 2.6 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -88,7 +88,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitmsg='poppler 21.11.0 rebuild'
+	commitmsg='openldap 2.6 rebuild'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
