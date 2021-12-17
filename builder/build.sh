d3e6f51207fda2ce3aa4b9c82a22544d590d0123
Evangelos Foutras <evangelos@foutrelis.com>
2021-12-17 07:52:10 +0200

    Prepare boost 1.78.0 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -88,7 +88,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitmsg='Python 3.10 rebuild'
+	commitmsg='boost 1.78.0 rebuild'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
