52b6aceb4a09d068d965be5d74e8c48461085517
Evangelos Foutras <evangelos@foutrelis.com>
2021-12-22 22:26:08 +0200

    Prepare protobuf 3.19 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -88,7 +88,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitmsg='boost 1.78.0 rebuild'
+	commitmsg='protobuf 3.19 rebuild'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
