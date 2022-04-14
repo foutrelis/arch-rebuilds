79631f9b05a7719dee3c957c5a172cd593d2b2ac
Evangelos Foutras <evangelos@foutrelis.com>
2022-04-14 17:13:03 +0300

    Prepare icu 71.1 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -88,7 +88,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitmsg='signed by 0x98BC6FF5 rebuild'
+	commitmsg='icu 71.1 rebuild'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
