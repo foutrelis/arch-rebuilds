4c120a15607a7a82fa9df1d270df1f09bdb95d1c
Evangelos Foutras <evangelos@foutrelis.com>
2022-06-11 16:27:35 +0300

    Prepare protobuf 3.21 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -88,7 +88,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitmsg='Perl 5.36 rebuild'
+	commitmsg='protobuf 3.21 rebuild'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
