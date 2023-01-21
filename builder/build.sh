6016245e97e0c22d5a3ceb4d08eba2d8bde7a292
Evangelos Foutras <evangelos@foutrelis.com>
2023-01-21 14:17:38 +0200

    Prepare boost 1.81.0 rebuild

--- builder/build.sh
+++ builder/build.sh
@@ -88,7 +88,7 @@ try_build() {
 
 	cd $base/trunk
 	setconf PKGBUILD pkgrel+=1
-	commitmsg='libtiff 4.5.0 rebuild'
+	commitmsg='boost 1.81.0 rebuild'
 
 	if [[ ${#repos[@]} -gt 1 ]]; then
 		api_call update base=$base status=failed log='Package exists in multiple repos'
